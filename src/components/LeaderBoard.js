7512a92088b363c1071cbbf40a19d25b0d7e2023
Jason Watkins <goushoryuken@gmail.com>
2022-08-17 13:43:38 -0700

    styling

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -49,28 +49,30 @@ const LeaderBoard = () => {
   return (
     <div className="leader-board">
       <Header />
-      <h1>leader board</h1>
-      <div className="score-item">
-        <h3>player</h3><h3>score</h3>
+      <div className="board-container">
+        <h1>leader board</h1>
+        <div className="score-item">
+          <h3>player</h3><h3>score</h3>
+        </div>
+        <div className="score-title">
+          <h4>level: space</h4>
+        </div>
+        {spaceScores.map((score) => {
+          return drawScores(score);
+        })}
+        <div className="score-title">
+          <h4>level: hollywood</h4>
+        </div>
+        {hollywoodScores.map((score) => {
+          return drawScores(score);
+        })}
+        <div className="score-title">
+          <h4>level: factory</h4>
+        </div>
+        {factoryScores.map((score) => {
+          return drawScores(score);
+        })}
       </div>
-      <div className="score-item">
-        <h4>level: space</h4>
-      </div>
-      {spaceScores.map((score) => {
-        return drawScores(score);
-      })}
-      <div className="score-item">
-        <h4>level: hollywood</h4>
-      </div>
-      {hollywoodScores.map((score) => {
-        return drawScores(score);
-      })}
-      <div className="score-item">
-        <h4>level: factory</h4>
-      </div>
-      {factoryScores.map((score) => {
-        return drawScores(score);
-      })}
     </div>
   )
 }
--- src/components/Select.js
+++ src/components/Select.js
@@ -7,10 +7,11 @@ import { Link } from 'react-router-dom'
 
 const Select = () => {
 
-  const [levels, setLevels] = useState([]);
+  const [space, setSpace] = useState({});
+  const [hollywood, setHollywood] = useState({});
+  const [factory, setFactory] = useState({});
   const loadLevels = () => {
     const url = "https://frozen-badlands-89928.herokuapp.com/api/v1/levels";
-    var tempLevels = [];
     fetch(url)
       .then((data) => {
         if (data.ok) {
@@ -21,9 +22,13 @@ const Select = () => {
       })
       .then((data) => {
         data.forEach((level) => {
-          console.log(level);
-          tempLevels.push(level.name);
-          setLevels(tempLevels);
+          if (level.id === 1) {
+            setSpace(level);
+          } else if (level.id === 2) {
+            setHollywood(level);
+          } else if (level.id === 3) {
+            setFactory(level);
+          }
         })
       })
   }
@@ -37,9 +42,11 @@ const Select = () => {
       <Header />
       <div className="select-main">
         <h2>select a level</h2>
-        <p>{levels}</p>
+        <div className="select-info"><span>level name: {space.name}</span><span>high score:</span></div>
         <Link to="/game" state={{levelID: 1}}><img className="select-icon" src={Space} /></Link>
+        <div className="select-info"><span>level name: {hollywood.name}</span><span>high score:</span></div>
         <Link to="/game" state={{levelID: 2}}><img className="select-icon" src={Hollywood} /></Link>
+        <div className="select-info"><span>level name: {factory.name}</span><span>high score:</span></div>
         <Link to="/game" state={{levelID: 3}}><img className="select-icon" src={Factory} /></Link>
       </div>
     </div>
