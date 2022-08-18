f4328515137ef5ae0956061f55c5a098cf5b8a3a
Jason Watkins <goushoryuken@gmail.com>
2022-08-17 17:23:56 -0700

    header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header><Link to="/" ><h1>wheres the guy</h1></ Link></header>
+    <header>
+      <Link to="/" ><h1>wheres the guy</h1></ Link>
+      <img className="fill-image" />
+      <img className="fill-image" />
+      <img className="fill-image" />
+      <img className="fill-image" />
+      <img className="fill-image" />
+    </header>
   )
 }
 
